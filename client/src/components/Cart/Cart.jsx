4d244ff692840d13f9305bf8f063fc5cf53036fd
Bezniak <ivan.bezniak@gmail.com>
2024-02-23 15:51:08 +0300

    shopping cart redux, reduxToolkit

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -1,53 +1,41 @@
 import React from 'react';
 import './Cart.scss';
 import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
+import {useDispatch, useSelector} from "react-redux";
+import {removeItem, resetCart} from "../../redux/cartReducer";
 
 
 const Cart = () => {
 
-    const data = [
-        {
-            id: 1,
-            img: "https://sgfm.elcorteingles.es/SGFM/dctm/MEDIA03/202312/26/00119316800523____6__516x640.jpg",
-            img2: "https://msmerimak.com/cdn/shop/files/pantalon-logan-camel_1.jpg?v=1694979229&width=533",
-            title: "Long Sleeve T-shirt",
-            desc: `Lorem ipsum dolor`,
-            isNew: true,
-            oldPrice: 19,
-            price: 12,
-        },
-        {
-            id: 2,
-            img: "https://img.kwcdn.com/product/Fancyalgo/VirtualModelMatting/19a847d5c9f1bc9cbb5d93f1c6d891c5.jpg?imageView2/2/w/500/q/60/format/webp",
-            img2: "https://m.media-amazon.com/images/I/51qGoaiqQXL._AC_UF1000,1000_QL80_.jpg",
-            title: "Long",
-            desc: `Accusantium aspernatur autem deserunt ipsam iure qui sint velit`,
-            isNew: false,
-            oldPrice: 19,
-            price: 12,
-        },
-    ]
+    const products = useSelector(state => state.cart.products)
+    const dispatch = useDispatch();
+
+    const totalPrice = () => {
+        let total = 0;
+        products.forEach((item) => (total += item.quantity * item.price));
+        return total.toFixed(2)
+    }
 
     return (
         <div className='cart'>
             <h1>Products in your cart</h1>
-            {data.map(item => (
+            {products?.map(item => (
                 <div className='item' key={item.id}>
-                    <img src={item.img} alt="photo"/>
+                    <img src={process.env.REACT_APP_UPLOAD_URL + item.img} alt="photo"/>
                     <div className="details">
                         <h1>{item.title}</h1>
                         <p>{item.desc?.substring(0, 80)}...</p>
-                        <div className='price'>1 x ${item.price}</div>
+                        <div className='price'>{item.quantity} x ${item.price}</div>
                     </div>
-                    <DeleteOutlineIcon className='delete'/>
+                    <DeleteOutlineIcon className='delete' onClick={() => dispatch(removeItem(item.id))}/>
                 </div>
             ))}
             <div className="total">
                 <span>SUBTOTAL</span>
-                <span>$123</span>
+                <span>${totalPrice()}</span>
             </div>
             <button>PROCEED TO CHECKOUT</button>
-            <span className='reset'>Reset Cart</span>
+            <span className='reset' onClick={() => dispatch(resetCart())}>Reset Cart</span>
         </div>
     );
 };
--- client/src/pages/Product/Product.jsx
+++ client/src/pages/Product/Product.jsx
@@ -6,13 +6,15 @@ import BalanceIcon from '@mui/icons-material/Balance';
 import useFetch from "../../hooks/useFetch";
 import {useParams} from "react-router-dom";
 import {Preloader} from "../../components/Preloader/Preloader";
+import {useDispatch} from "react-redux";
+import {addToCart} from "../../redux/cartReducer";
 
 const Product = () => {
 
     const id = useParams().id
     const [selectedImg, setSelectedImg] = useState('img')
     const [quantity, setQuantity] = useState(1)
-
+    const dispatch = useDispatch()
     const {data, loading, error} = useFetch(`/products/${id}?populate=*`);
 
     return (
@@ -28,24 +30,22 @@ const Product = () => {
                                     alt="img"
                                     onClick={e => setSelectedImg("img")}
                                 />
-                                <img src={process.env.REACT_APP_UPLOAD_URL + data?.attributes?.img2?.data?.attributes?.url}
-                                     alt="img"
-                                     onClick={e => setSelectedImg("img2")}
+                                <img
+                                    src={process.env.REACT_APP_UPLOAD_URL + data?.attributes?.img2?.data?.attributes?.url}
+                                    alt="img"
+                                    onClick={e => setSelectedImg("img2")}
                                 />
                             </div>
                             <div className="mainImg">
-                                <img src={process.env.REACT_APP_UPLOAD_URL + data?.attributes[selectedImg].data?.attributes?.url} alt="photo"/>
+                                <img
+                                    src={process.env.REACT_APP_UPLOAD_URL + data?.attributes[selectedImg].data?.attributes?.url}
+                                    alt="photo"/>
                             </div>
                         </div>
                         <div className="right">
-                            <h1>Title</h1>
-                            <span className='price'>$199</span>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, accusantium cumque dolores
-                                excepturi
-                                exercitationem explicabo fugiat laborum minus nulla odio omnis quam recusandae,
-                                reprehenderit
-                                sapiente sed suscipit tempora tenetur voluptate.
-                            </p>
+                            <h1>{data?.attributes?.title}</h1>
+                            <span className='price'>${data?.attributes?.price}</span>
+                            <p>{data?.attributes?.desc}</p>
                             <div className="quantity">
                                 <button
                                     onClick={() => setQuantity(prevState => prevState === 1 ? 1 : prevState - 1)}>-
@@ -53,7 +53,14 @@ const Product = () => {
                                 {quantity}
                                 <button onClick={() => setQuantity(prevState => prevState + 1)}>+</button>
                             </div>
-                            <button className='add'>
+                            <button className='add' onClick={() => dispatch(addToCart({
+                                id: data.id,
+                                title: data.attributes.title,
+                                desc: data.attributes.desc,
+                                price: data.attributes.price,
+                                img: data.attributes.img.data.attributes.url,
+                                quantity,
+                            }))}>
                                 <AddShoppingCartIcon/> ADD TO CART
                             </button>
                             <div className="links">
